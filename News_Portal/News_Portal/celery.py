3d1a35a6fb10fba673a3047050ade9e5d55e3047
shalkov-i <117475785+shalkov-i@users.noreply.github.com>
2023-03-19 09:32:11 +0500

    Delete News_Portal directory

--- News_Portal/News_Portal/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'News_Portal.settings')
-
-app = Celery('News_Portal')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-app.autodiscover_tasks()
--- News_Portal/News_Portal_app/__init__.py
+++
@@ -1,3 +0,0 @@
-from News_Portal.celery import app as celery_app
-
-__all__ = ('celery_app',)
